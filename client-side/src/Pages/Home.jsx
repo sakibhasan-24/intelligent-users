75346a918a4f7253a8ba486aa7bb39704577fb05
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-11-27 00:55:17 +0600

    add server

--- client-side/src/Pages/Home.jsx
+++ client-side/src/Pages/Home.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <div>Homes</div>;
+  return (
+    <header className="bg-slate-200">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
+        <Link to="">
+          <h1 className="font-bold">Users</h1>
+        </Link>
+        <ul className="flex space-x-4 font-bold">
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/signup">
+            <li>SignUp</li>
+          </Link>
+          <Link to="/login">
+            <li>Login</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
 }
