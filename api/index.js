5a816b5ebb0c09996bf1432296d91fcead043ed7
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-11-27 01:15:15 +0600

    connected mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
+const url = process.env.MONGO_URI;
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((e) => console.log(e));
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
