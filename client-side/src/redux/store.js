49133d2060645d28f16b485070f76bc10f9787eb
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-11-30 00:32:18 +0600

    add local storage using persist

--- client-side/src/redux/store.js
+++ client-side/src/redux/store.js
@@ -1,11 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userStorage.js";
-
+import { persistReducer } from "redux-persist";
+// import { storage } from "redux-persist/lib/storage";
+import storage from "redux-persist/lib/storage";
+// redux-persist/lib/storage
+import persistStore from "redux-persist/es/persistStore";
+const rootReducer = combineReducers({ user: userReducer });
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
-  reducer: { user: userReducer },
+  // reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
 });
+
+export const persistor = persistStore(store);
 
 // Reducers in Redux are responsible for handling actions and updating the state of the application
 /* 
