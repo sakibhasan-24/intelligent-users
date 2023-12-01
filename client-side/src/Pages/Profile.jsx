2de234cedd9c26e926a734ceebdd7ecd9bb9f5f8
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-12-01 11:54:06 +0600

    add image functionality for uplaod firebase

--- client-side/src/Pages/Profile.jsx
+++ client-side/src/Pages/Profile.jsx
@@ -1,19 +1,91 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import app from "../../firebase/firebase.config";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
 
 export default function Profile() {
+  const fileRef = useRef(null);
   const { currentUser, loading } = useSelector((state) => state.user);
+  const [image, setImage] = useState(undefined);
+  const [imageProgress, setImageProgress] = useState(0);
+  const [imageError, setImageError] = useState(false);
+  const [formData, setFormData] = useState({});
+  //   console.log(image);
   const handleFormData = (e) => {
     e.preventDefault();
   };
 
+  useEffect(() => {
+    if (image) {
+      // uploadImage
+      uploadImage(image);
+    }
+  }, [image]);
+  const uploadImage = async (image) => {
+    // console.log(image);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageProgress(Math.round(progress));
+        switch (snapshot.state) {
+          case "paused":
+            // console.log("Upload is paused");
+            break;
+          case "running":
+            // console.log("Upload is running");
+            break;
+        }
+      },
+      (error) => {
+        setImageError(true);
+        switch (error.code) {
+          case "storage/unauthorized":
+            break;
+          case "storage/canceled":
+            break;
+
+          case "storage/unknown":
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          //   console.log("File available at", downloadURL);
+          setFormData({ ...formData, downloadURL });
+        });
+      }
+    );
+  };
+  //   console.log(imageProgress);
   return (
     <div className="max-w-4xl mx-auto ">
+      <input
+        type="file"
+        ref={fileRef}
+        name=""
+        id="file"
+        className="hidden"
+        accept=""
+        onChange={(e) => setImage(e.target.files[0])}
+      />
       <h1 className="text-center font-bold text-lg gap-6  my-10">Profile</h1>
       <form className="flex flex-col max-w-2xl  mx-auto my-6 shadow-lg  ">
         <img
           src={currentUser.profilePicture}
           alt=""
+          onClick={() => fileRef.current.click()}
           className="h-20 w-20 cursor-pointer self-center rounded-full object-cover"
         />
         <input
